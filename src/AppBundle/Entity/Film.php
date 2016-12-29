ab6002d97a11634e21ccf5356d33da8ac775e655
benjarr <rabenjasolo@gmail.com>
2016-12-29 16:50:57 +0300

    construction des entites

---
+++ src/AppBundle/Entity/Film.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Film
+ *
+ * @ORM\Table(name="film")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\FilmRepository")
+ */
+class Film
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Categorie")
+     */
+    private $categorie;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Acteur")
+     */
+    private $acteurs;
+
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->acteurs = new ArrayCollection();
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Film
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Film
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set categorie
+     *
+     * @param \AppBundle\Entity\Categorie $categorie
+     *
+     * @return Film
+     */
+    public function setCategorie(Categorie $categorie = null)
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    /**
+     * Get categorie
+     *
+     * @return \AppBundle\Entity\Categorie
+     */
+    public function getCategorie()
+    {
+        return $this->categorie;
+    }
+
+    /**
+     * Add acteur
+     *
+     * @param \AppBundle\Entity\Acteur $acteur
+     *
+     * @return Film
+     */
+    public function addActeur(Acteur $acteur)
+    {
+        $this->acteurs[] = $acteur;
+
+        return $this;
+    }
+
+    /**
+     * Remove acteur
+     *
+     * @param \AppBundle\Entity\Acteur $acteur
+     */
+    public function removeActeur(Acteur $acteur)
+    {
+        $this->acteurs->removeElement($acteur);
+    }
+
+    /**
+     * Get acteurs
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getActeurs()
+    {
+        return $this->acteurs;
+    }
+}
